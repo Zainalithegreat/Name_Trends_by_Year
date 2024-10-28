2df56876ec2a881947b02e319c194e5cadf66b4c
Zainalithegreat <zain.ali@pcc.edu>
2024-10-28 03:22:10 -0700

    First Commit

---
+++ main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Names</title>
+</head>
+<body>
+<h1>Swimming Pool Calculator</h1>
+<p>How much water will it take to fill an above-ground pool?</p>
+<form method="get" action="names.php">
+    <table>
+        <tr>
+            <td><label>Enter the year:</label></td>
+            <td><input type="text" id="year" name="year" style="width: 100px;"></td>
+        </tr>
+        <tr>
+            <td><label>Enter the gender:</label></td>
+            <td><input type="text" id="gender" name="gender" style="width: 100px;"></td>
+        </tr>
+        <tr>
+            <td><label>Enter the count (all if you want to include all):</label></td>
+            <td><input type="text" id="count" name="count" style="width: 100px;"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="Submit"></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
